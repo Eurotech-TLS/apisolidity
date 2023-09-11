a014326580ee894dd29dfb9d0261bcff8bafa324
clafuenteEurotech <clafuente@eurotechtls.eu>
2023-09-11 17:12:51 +0200

    First push

---
+++ src/main/java/eu/eurotechtls/geotrack/apisolidity/ApisolidityApplication.java
@@ -0,0 +1,13 @@
+package eu.eurotechtls.geotrack.apisolidity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisolidityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisolidityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/eurotechtls/geotrack/apisolidity/ApisolidityApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.eurotechtls.geotrack.apisolidity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisolidityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
